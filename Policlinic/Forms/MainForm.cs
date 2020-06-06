15bde21a5ce48bca2a2cdf8c2e32bebe5ff921ae
Dmitry Sharonov <dmitry_sharonov@epam.com>
2020-06-06 23:45:56 +0300

    Add coupons print

--- Policlinic/Forms/MainForm.cs
+++ Policlinic/Forms/MainForm.cs
@@ -9,24 +9,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Printing;
 
 namespace Forms
 {
     public partial class MainForm : Form
     {
         BusinessLogic bl = new BusinessLogic();
+        private string print;
+        private string[] textPrint;
 
         public MainForm()
         {
             InitializeComponent();
-
-            bl.GetPatientsList();
-
-            foreach (var category in bl.GetCategoriesList())
-            {
-                comboReceptionCategory.Items.Add(category.Name.ToString());
-            }
-            
         }
 
         public void PatientsGridRenew()
@@ -105,9 +100,100 @@ namespace Forms
             freeCouponsGridView.Rows[rowIndex].ReadOnly = true;
         }
 
+        public void GridCouponDateRenew(DateTime dateAfter, DateTime dateBefore)
+        {
+            if (gridCouponDate.Rows.Count > 1)
+            {
+                gridCouponDate.Rows.Clear();
+            }
+            int rowIndex = 0;
+            foreach (var couponDate in bl.GetCouponsDateList(dateAfter, dateBefore))
+            {
+                gridCouponDate.Rows.Add();
+                gridCouponDate[0, rowIndex].Value = couponDate.TreatmentType;
+                gridCouponDate[1, rowIndex].Value = couponDate.Patient;
+                gridCouponDate[2, rowIndex].Value = couponDate.Doctor;
+                gridCouponDate[3, rowIndex].Value = couponDate.Specialty;
+                gridCouponDate[4, rowIndex].Value = string.Format("{0:d}", couponDate.Date);
+                gridCouponDate[5, rowIndex].Value = string.Format("{0:t}", couponDate.Time);
+                gridCouponDate[6, rowIndex].ReadOnly = false;
+                rowIndex++;
+            }
+        }
+
+        public void GridCouponDoctorRenew(DateTime dateAfter, DateTime dateBefore, string doctor)
+        {
+            if (gridCouponDoctor.Rows.Count > 1)
+            {
+                gridCouponDoctor.Rows.Clear();
+            }
+            int rowIndex = 0;
+            foreach (var couponDate in bl.GetCouponsDoctorList(dateAfter, dateBefore, doctor))
+            {
+                gridCouponDoctor.Rows.Add();
+                gridCouponDoctor[0, rowIndex].Value = couponDate.TreatmentType;
+                gridCouponDoctor[1, rowIndex].Value = couponDate.Patient;
+                gridCouponDoctor[2, rowIndex].Value = couponDate.Doctor;
+                gridCouponDoctor[3, rowIndex].Value = couponDate.Specialty;
+                gridCouponDoctor[4, rowIndex].Value = string.Format("{0:d}", couponDate.Date);
+                gridCouponDoctor[5, rowIndex].Value = string.Format("{0:t}", couponDate.Time);
+                gridCouponDoctor[6, rowIndex].ReadOnly = false;
+                rowIndex++;
+            }
+        }
+
+        public void GridCouponSpecialtyRenew(DateTime dateAfter, DateTime dateBefore, string specialty)
+        {
+            if (gridCouponSpecialty.Rows.Count > 1)
+            {
+                gridCouponSpecialty.Rows.Clear();
+            }
+            int rowIndex = 0;
+            foreach (var couponSpecialty in bl.GetCouponsSpecialtyList(dateAfter, dateBefore, specialty))
+            {
+                gridCouponSpecialty.Rows.Add();
+                gridCouponSpecialty[0, rowIndex].Value = couponSpecialty.TreatmentType;
+                gridCouponSpecialty[1, rowIndex].Value = couponSpecialty.Patient;
+                gridCouponSpecialty[2, rowIndex].Value = couponSpecialty.Doctor;
+                gridCouponSpecialty[3, rowIndex].Value = couponSpecialty.Specialty;
+                gridCouponSpecialty[4, rowIndex].Value = string.Format("{0:d}", couponSpecialty.Date);
+                gridCouponSpecialty[5, rowIndex].Value = string.Format("{0:t}", couponSpecialty.Time);
+                gridCouponSpecialty[6, rowIndex].ReadOnly = false;
+                rowIndex++;
+            }
+        }
+
         private void Main_Load(object sender, EventArgs e)
         {
             PatientsGridRenew();
+
+            foreach (var category in bl.GetCategoriesList())
+            {
+                comboReceptionCategory.Items.Add(category.Name.ToString());
+            }
+
+            foreach (var doctor in bl.GetDoctorsList())
+            {
+                comboBoxDoctor.Items.Add(doctor.Name.ToString());
+            }
+
+            foreach (var specialty in bl.GetSpecialtiesList())
+            {
+                comboBoxSpecialty.Items.Add(specialty.Name.ToString());
+            }
+
+            dateTimePicker2.MaxDate = DateTime.Today;
+            dateTimePicker2.Value = DateTime.Today;
+            dateTimePicker3.MaxDate = DateTime.Today;
+            dateTimePicker3.Value = DateTime.Today;
+            dateTimePicker4.MaxDate = DateTime.Today;
+            dateTimePicker4.Value = DateTime.Today;
+            dateTimePicker5.MaxDate = DateTime.Today;
+            dateTimePicker5.Value = DateTime.Today;
+            dateTimePicker6.MaxDate = DateTime.Today;
+            dateTimePicker6.Value = DateTime.Today;
+            dateTimePicker7.MaxDate = DateTime.Today;
+            dateTimePicker7.Value = DateTime.Today;
         }
 
         private void регистрацияToolStripMenuItem_Click(object sender, EventArgs e)
@@ -291,9 +377,9 @@ namespace Forms
                         freeCouponsGridView.Rows.Clear();
                         comboDoctor.Items.Clear();
                         comboTreatmentType.Items.Clear();
-                        comboReceptionCategory.Items.Clear();
-                        string[] text = bl.PrintCoupon(id).Split(',');
+                        string[] text = bl.PrintCouponSelect(id).Split(',');
                         listGetCouponMessage.Items.AddRange(text);
+                        textPrint = text;
                     }
                 }
             }
@@ -301,6 +387,240 @@ namespace Forms
             {
                 textPolicy.Focus();
                 errorProvider.SetError(textPolicy, "Некорректный формат полиса!");
+            }
+        }
+
+        private void buttonCouponDate_Click(object sender, EventArgs e)
+        {
+            if (DateTime.Compare(dateTimePicker2.Value, dateTimePicker3.Value) > 0)
+            {
+                errorProviderCouponDate.SetError(dateTimePicker2, "Некорректный период!");
+                errorProviderCouponDate.SetError(dateTimePicker3, "Некорректный период!");
+            }
+            else
+            {
+                errorProviderCouponDate.Clear();
+                GridCouponDateRenew(dateTimePicker2.Value, dateTimePicker3.Value);
+            }
+        }
+
+        private void buttonPrintDate_Click(object sender, EventArgs e)
+        {
+            int index = 0;
+            int count = 0;
+            foreach (DataGridViewRow row in gridCouponDate.Rows)
+            {
+                if (Convert.ToBoolean(row.Cells["ColumnSelect"].Value))
+                {
+                    index = row.Index;
+                    count++;
+                }
+            }
+            if (count < 1)
+            {
+                MessageBox.Show("Не выбран талон!");
+            }
+            else if (count > 1)
+            {
+                MessageBox.Show("Выберите один талон!");
+            }
+            else
+            {
+                string treatment = gridCouponDate[0, index].Value.ToString();
+                string patient = gridCouponDate[1, index].Value.ToString();
+                string doctor = gridCouponDate[2, index].Value.ToString();
+                DateTime date = DateTime.Parse(gridCouponDate[4, index].Value.ToString());
+                DateTime time = DateTime.Parse(gridCouponDate[5, index].Value.ToString());
+
+                string[] text = bl.PrintCoupon(patient, treatment, doctor, date, time).Split(',');
+                for (int i = 0; i < text.Length; i++)
+                {
+                    print = print + text[i] + "\n";
+                }
+
+                MessageBox.Show(print);
+
+                gridCouponDate.Rows.Clear();
+                dateTimePicker2.Value = DateTime.Today;
+                dateTimePicker3.Value = DateTime.Today;
+                PrintDocument printDocument = new PrintDocument();
+                printDocument.PrintPage += PrintPageHandler;
+                PrintDialog printDialog = new PrintDialog();
+                printDialog.Document = printDocument;
+                if (printDialog.ShowDialog() == DialogResult.OK)
+                    printDialog.Document.Print();
+            }
+        }
+
+        private void PrintPageHandler(object sender, PrintPageEventArgs e)
+        {
+            e.Graphics.DrawString(print, new Font("Arial", 14), Brushes.Black, 0, 0);
+        }
+
+        private void buttonSelect_Click(object sender, EventArgs e)
+        {
+            errorProviderCouponDoctor.Clear();
+            if (DateTime.Compare(dateTimePicker5.Value, dateTimePicker4.Value) > 0)
+            {
+                errorProviderCouponDoctor.SetError(dateTimePicker4, "Некорректный период!");
+                errorProviderCouponDoctor.SetError(dateTimePicker5, "Некорректный период!");
+            }
+            else
+            {
+                if (comboBoxDoctor.SelectedIndex > -1)
+                {
+                    errorProviderCouponDoctor.Clear();
+                    GridCouponDoctorRenew(dateTimePicker5.Value, dateTimePicker4.Value, comboBoxDoctor.SelectedItem.ToString());
+                }
+                else
+                {
+                    errorProviderCouponDoctor.SetError(comboBoxDoctor, "Не выбран врач!");
+                }
+            }
+            
+        }
+
+        private void buttonPrint_Click(object sender, EventArgs e)
+        {
+            int index = 0;
+            int count = 0;
+            foreach (DataGridViewRow row in gridCouponDoctor.Rows)
+            {
+                if (Convert.ToBoolean(row.Cells["ColumnSelect1"].Value))
+                {
+                    index = row.Index;
+                    count++;
+                }
+            }
+            if (count < 1)
+            {
+                MessageBox.Show("Не выбран талон!");
+            }
+            else if (count > 1)
+            {
+                MessageBox.Show("Выберите один талон!");
+            }
+            else
+            {
+                string treatment = gridCouponDoctor[0, index].Value.ToString();
+                string patient = gridCouponDoctor[1, index].Value.ToString();
+                string doctor = gridCouponDoctor[2, index].Value.ToString();
+                DateTime date = DateTime.Parse(gridCouponDoctor[4, index].Value.ToString());
+                DateTime time = DateTime.Parse(gridCouponDoctor[5, index].Value.ToString());
+
+                string[] text = bl.PrintCoupon(patient, treatment, doctor, date, time).Split(',');
+                for (int i = 0; i < text.Length; i++)
+                {
+                    print = print + text[i] + "\n";
+                }
+
+                MessageBox.Show(print);
+
+                gridCouponDoctor.Rows.Clear();
+                dateTimePicker4.Value = DateTime.Today;
+                dateTimePicker5.Value = DateTime.Today;
+                PrintDocument printDocument = new PrintDocument();
+                printDocument.PrintPage += PrintPageHandler;
+                PrintDialog printDialog = new PrintDialog();
+                printDialog.Document = printDocument;
+                if (printDialog.ShowDialog() == DialogResult.OK)
+                    printDialog.Document.Print();
+                print = string.Empty;
+            }
+        }
+
+        private void buttonSeleceCoupon_Click(object sender, EventArgs e)
+        {
+            if (DateTime.Compare(dateTimePicker7.Value, dateTimePicker6.Value) > 0)
+            {
+                errorProviderCouponSpecialty.SetError(dateTimePicker6, "Некорректный период!");
+                errorProviderCouponSpecialty.SetError(dateTimePicker7, "Некорректный период!");
+            }
+            else
+            {
+                errorProviderCouponSpecialty.Clear();
+                if (comboBoxSpecialty.SelectedIndex > -1)
+                {
+                    errorProviderCouponSpecialty.Clear();
+                    GridCouponSpecialtyRenew(dateTimePicker7.Value, dateTimePicker6.Value, comboBoxSpecialty.SelectedItem.ToString());
+                }
+                else
+                {
+                    errorProviderCouponSpecialty.SetError(comboBoxSpecialty, "Не выбрана специализация!");
+                }
+            }
+        }
+
+        private void buttonPrintCouponDoctor_Click(object sender, EventArgs e)
+        {
+            int index = 0;
+            int count = 0;
+            foreach (DataGridViewRow row in gridCouponSpecialty.Rows)
+            {
+                if (Convert.ToBoolean(row.Cells["ColumnSelectCoupon"].Value))
+                {
+                    index = row.Index;
+                    count++;
+                }
+            }
+            if (count < 1)
+            {
+                MessageBox.Show("Не выбран талон!");
+            }
+            else if (count > 1)
+            {
+                MessageBox.Show("Выберите один талон!");
+            }
+            else
+            {
+                string treatment = gridCouponSpecialty[0, index].Value.ToString();
+                string patient = gridCouponSpecialty[1, index].Value.ToString();
+                string doctor = gridCouponSpecialty[2, index].Value.ToString();
+                DateTime date = DateTime.Parse(gridCouponSpecialty[4, index].Value.ToString());
+                DateTime time = DateTime.Parse(gridCouponSpecialty[5, index].Value.ToString());
+                string[] text = bl.PrintCoupon(patient, treatment, doctor, date, time).Split(',');
+                for (int i = 0; i < text.Length; i++)
+                {
+                    print = print + text[i] + "\n";
+                }
+
+                MessageBox.Show(print);
+
+                gridCouponDoctor.Rows.Clear();
+                dateTimePicker6.Value = DateTime.Today;
+                dateTimePicker7.Value = DateTime.Today;
+                PrintDocument printDocument = new PrintDocument();
+                printDocument.PrintPage += PrintPageHandler;
+                PrintDialog printDialog = new PrintDialog();
+                printDialog.Document = printDocument;
+                if (printDialog.ShowDialog() == DialogResult.OK)
+                    printDialog.Document.Print();
+                print = string.Empty;
+            }
+        }
+
+        private void buttonPrintCoupon_Click(object sender, EventArgs e)
+        {
+            if (listGetCouponMessage.Text != "")
+            {
+                MessageBox.Show("Талон не выписан!");
+            }
+            else
+            {
+                for (int i = 0; i < textPrint.Length; i++)
+                {
+                    print = print + textPrint[i] + "\n";
+                }
+
+                MessageBox.Show(print);
+
+                PrintDocument printDocument = new PrintDocument();
+                printDocument.PrintPage += PrintPageHandler;
+                PrintDialog printDialog = new PrintDialog();
+                printDialog.Document = printDocument;
+                if (printDialog.ShowDialog() == DialogResult.OK)
+                    printDialog.Document.Print();
+                print = string.Empty;
             }
         }
     }
